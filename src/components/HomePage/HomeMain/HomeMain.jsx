5588cadd25105287db855f23634b71e80f5ba845
Hripsime <m_hripsime@mail.ru>
2025-01-06 23:53:33 +0400

    Added card columns

--- src/components/HomePage/HomeMain/HomeMain.jsx
+++ src/components/HomePage/HomeMain/HomeMain.jsx
@@ -4,25 +4,194 @@ const HomeMain = () => {
   return (
     <>
       <section className={styles.homeMain}>
-         <div className={styles.homeMain__cardContainer}>
-         <div className={styles.homeMain__cardContainer__card}>
-         <div className={styles.homeMain__cardContainer__card_item}>1</div>
-         <div className={styles.homeMain__cardContainer__card_item}>2</div>
+        <div className={styles.homeMain__cardContainer}>
+          <div className={styles.homeMain__cardContainer__card}>
+            <div className={styles.homeMain__cardContainer__card_item}>
+          
+            <div className={styles.homeMain__cardContainer__card_item}>
+              <div className={styles.homeMain__cardContainer__card_gameInfo}>
+              <div className={styles.homeMain__cardContainer__card_gameNameBlock}>
+                
+                <h2 className={styles.homeMain__cardContainer__card_gameName}>
+                  Loftia VR Game
+                </h2>
+
+                <span className={styles.homeMain__cardContainer__card_videogame}>
+                  video game
+                </span>
+                </div>
+                <div className={styles.homeMain__cardContainer__card_position}>top</div>
+              </div>             
+              </div>            
+            </div>
+
+
+            <div className={styles.homeMain__cardContainer__card_item}>
+            game status
+            </div>
+          </div> {/* //card*/}
+
+
+
+
+
+
+         
+          <div className={styles.homeMain__cardContainer__card2}>
+            <div className={styles.homeMain__cardContainer__card_item}>
+          
+            <div className={styles.homeMain__cardContainer__card_item}>
+              <div className={styles.homeMain__cardContainer__card_gameInfo}>
+              <div className={styles.homeMain__cardContainer__card_gameNameBlock}>
+                
+                <h2 className={styles.homeMain__cardContainer__card_gameName}>
+                Design System
+                <br /
+                > Automation
+                </h2>
+
+                <span className={styles.homeMain__cardContainer__card_videogame}>
+                  video game
+                </span>
+                </div>
+                <div className={styles.homeMain__cardContainer__card_position}>top</div>
+              </div>             
+              </div>            
+            </div>
+
+
+            <div className={styles.homeMain__cardContainer__card_item}>
+            game status
+            </div>
+          </div> {/* //card*/}
+
+
+
+
+          <div className={styles.homeMain__cardContainer__card}>
+            <div className={styles.homeMain__cardContainer__card_item}>
+          
+            <div className={styles.homeMain__cardContainer__card_item}>
+              <div className={styles.homeMain__cardContainer__card_gameInfo}>
+              <div className={styles.homeMain__cardContainer__card_gameNameBlock}>
+                
+                <h2 className={styles.homeMain__cardContainer__card_gameName}>
+                Loftia VR Game
+                </h2>
+
+                <span className={styles.homeMain__cardContainer__card_videogame}>
+                  video game
+                </span>
+                </div>
+                <div className={styles.homeMain__cardContainer__card_position}>top</div>
+              </div>             
+              </div>            
+            </div>
+
+
+            <div className={styles.homeMain__cardContainer__card_item}>
+            game status
+            </div>
+          </div> {/* //card*/}
+
+
+{/* Second row */}
  
 
+<div className={styles.homeMain__cardContainer__card}>
+            <div className={styles.homeMain__cardContainer__card_item}>
+          
+            <div className={styles.homeMain__cardContainer__card_item}>
+              <div className={styles.homeMain__cardContainer__card_gameInfo}>
+              <div className={styles.homeMain__cardContainer__card_gameNameBlock}>
+                
+                <h2 className={styles.homeMain__cardContainer__card_gameName}>
+                  Loftia VR Game
+                </h2>
+
+                <span className={styles.homeMain__cardContainer__card_videogame}>
+                  video game
+                </span>
+                </div>
+                <div className={styles.homeMain__cardContainer__card_position}>top</div>
+              </div>             
+              </div>            
+            </div>
 
 
-         </div>
+            <div className={styles.homeMain__cardContainer__card_item}>
+            game status
+            </div>
+          </div> {/* //card*/}
 
-         <div className={styles.homeMain__cardContainer__card}>
-         2
-         </div>
 
-         <div className={styles.homeMain__cardContainer__card}>
-         3
-         </div>
 
-         </div>
+
+
+
+         
+          <div className={styles.homeMain__cardContainer__card2}>
+            <div className={styles.homeMain__cardContainer__card_item}>
+          
+            <div className={styles.homeMain__cardContainer__card_item}>
+              <div className={styles.homeMain__cardContainer__card_gameInfo}>
+              <div className={styles.homeMain__cardContainer__card_gameNameBlock}>
+                
+                <h2 className={styles.homeMain__cardContainer__card_gameName}>
+                Design System
+                <br /
+                > Automation
+                </h2>
+
+                <span className={styles.homeMain__cardContainer__card_videogame}>
+                  video game
+                </span>
+                </div>
+                <div className={styles.homeMain__cardContainer__card_position}>top</div>
+              </div>             
+              </div>            
+            </div>
+
+
+            <div className={styles.homeMain__cardContainer__card_item}>
+            game status
+            </div>
+          </div> {/* //card*/}
+
+
+
+
+          <div className={styles.homeMain__cardContainer__card}>
+            <div className={styles.homeMain__cardContainer__card_item}>
+          
+            <div className={styles.homeMain__cardContainer__card_item}>
+              <div className={styles.homeMain__cardContainer__card_gameInfo}>
+              <div className={styles.homeMain__cardContainer__card_gameNameBlock}>
+                
+                <h2 className={styles.homeMain__cardContainer__card_gameName}>
+                Loftia VR Game
+                </h2>
+
+                <span className={styles.homeMain__cardContainer__card_videogame}>
+                  video game
+                </span>
+                </div>
+                <div className={styles.homeMain__cardContainer__card_position}>top</div>
+              </div>             
+              </div>            
+            </div>
+
+
+            <div className={styles.homeMain__cardContainer__card_item}>
+            game status
+            </div>
+          </div> {/* //card*/}
+
+
+
+
+
+        </div>
       </section>
     </>
   );
