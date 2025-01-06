62c68fff4b1f5d51fbe03ce4f1a0bc6a15437fab
Hripsime <m_hripsime@mail.ru>
2025-01-06 21:44:52 +0400

    Added card structure

--- src/assets/images.js
+++ src/assets/images.js
@@ -3,8 +3,14 @@
 //import robotMonitorImage from "./images/home/robotMonitor.png"
 //import robotMonitorWebpImage from "./images/home/robotMonitor.webp"
 
-import avatarImage from "./images/all/avatar.jpg";
+import avatarImage from "./images/header/avatar.jpg";
+/*
+import videogame1Image from "./images/home/videogame1.jpg"
+import videogame1WebpImage from "./images/home/videogame1.webp"
 
+import videogame2Image from "./images/home/videogame2.jpg"
+import videogame2WebpImage from "./images/home/videogame2.webp"
+*/
 export {
   // Home
 
@@ -12,4 +18,11 @@ export {
   // robotMonitorWebpImage,
 
   avatarImage,
+/*
+  videogame1Image,
+  videogame1WebpImage,
+
+  videogame2Image,
+  videogame2WebpImage,
+*/
 };
