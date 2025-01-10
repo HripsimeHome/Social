ab5bcdfef2f3f3d9db8cffe51d73682f7919bdd1
Hripsime <m_hripsime@mail.ru>
2025-01-10 18:54:49 +0400

    Added rogressar

--- src/components/HomePage/HomeMain/HomeMain.jsx
+++ src/components/HomePage/HomeMain/HomeMain.jsx
@@ -1,85 +1,59 @@
 import styles from "./HomeMain.module.scss";
+import ProgressBarFunding from "../../layout/ProgressBarFunding/ProgressBarFunding"
 
 const HomeMain = () => {
   return (
     <>
       <section className={styles.homeMain}>
-        <div className={styles.homeMain__cardContainer}>
-
-          <div className={styles.homeMain__cardContainer__card}> 
-            <div className={styles.homeMain__cardContainer__card_gameInfo}>
-              <div className={styles.homeMain__cardContainer__card_gameNameBlock}>
-                <h2 className={styles.homeMain__cardContainer__card_gameName}>
-                  Loftia VR Game
-                </h2>
-                <span className={styles.homeMain__cardContainer__card_videogame}>
-                  video game
-                </span>
-              </div> {/*  /gameNameBlock*/}
-              <div className={styles.homeMain__cardContainer__card_position}>top</div>
-            </div>  {/*  /gameInfo*/}    
-                 
-            <div className={styles.homeMain__cardContainer__card_gameStatus}>
-              <div className={styles.homeMain__cardContainer__card_funded}>
-              <div className={styles.homeMain__cardContainer__card_funded}>
-                <span>40%</span>
-                <span>funded</span>
-                </div>
+        <div className={styles.homeMain__card}> 
+          <div className={styles.homeMain__cardHeader}>
+            <div className={styles.homeMain__cardGameNameBlock}>
+              <h2 className={styles.homeMain__cardGameName}>
+                Loftia VR Game
+              </h2>
+              <span className={styles.homeMain__cardVideogame}>
+                video game
+              </span>
+            </div>  
+            <div className={styles.homeMain__cardGamePosition}>top</div>
+          </div>      
                 
-                <span>15 backers</span>
-              </div>
-
-              <span>34 days to go</span>
-            </div>
-          </div> {/* /card*/}
+          <ProgressBarFunding />
+        </div> {/* /card*/}
 
 
-          <div className={styles.homeMain__cardContainer__card2}> 
-            <div className={styles.homeMain__cardContainer__card_gameInfo}>
-              <div className={styles.homeMain__cardContainer__card_gameNameBlock}>
-                <h2 className={styles.homeMain__cardContainer__card_gameName}>
-                Design System
-                <br />
-                Automation
-                </h2>
-                <span className={styles.homeMain__cardContainer__card_videogame}>
-                  video game
-                </span>
-              </div> {/*  /gameNameBlock*/}
-              <div className={styles.homeMain__cardContainer__card_position}>top</div>
-            </div>  {/*  /gameInfo*/}    
-                 
-            <div className={styles.homeMain__cardContainer__card_gameStatus}>
-              <div className={styles.homeMain__cardContainer__card_funded}>
-                <span>funded</span>
-                <span>15 backers</span>
-              </div>
-              <span>34 days to go</span>
-            </div>
-          </div> {/* /card*/}
+        <div className={styles.homeMain__card2}> 
+          <div className={styles.homeMain__cardHeader}>
+            <div className={styles.homeMain__cardGameNameBlock}>
+              <h2 className={styles.homeMain__cardGameName}>
+                Loftia VR Game
+              </h2>
+              <span className={styles.homeMain__cardVideogame}>
+                video game
+              </span>
+            </div>  
+            <div className={styles.homeMain__cardGamePosition}>top</div>
+          </div>      
+                
+          <ProgressBarFunding />
+        </div> {/* /card*/}
 
 
-          <div className={styles.homeMain__cardContainer__card}> 
-            <div className={styles.homeMain__cardContainer__card_gameInfo}>
-              <div className={styles.homeMain__cardContainer__card_gameNameBlock}>
-                <h2 className={styles.homeMain__cardContainer__card_gameName}>
-                  Loftia VR Game
-                </h2>
-                <span className={styles.homeMain__cardContainer__card_videogame}>
-                  video game
-                </span>
-              </div> {/*  /gameNameBlock*/}
-              <div className={styles.homeMain__cardContainer__card_position}>top</div>
-            </div>  {/*  /gameInfo*/}    
-                 
-            <div className={styles.homeMain__cardContainer__card_gameStatus}>
-              <div className={styles.homeMain__cardContainer__card_funded}>
-                <span>funded</span>
-                <span>15 backers</span>
-              </div>
-              <span>34 days to go</span>
-            </div>
-          </div> {/* /card*/}
+        <div className={styles.homeMain__card}> 
+          <div className={styles.homeMain__cardHeader}>
+            <div className={styles.homeMain__cardGameNameBlock}>
+              <h2 className={styles.homeMain__cardGameName}>
+                Loftia VR Game
+              </h2>
+              <span className={styles.homeMain__cardVideogame}>
+                video game
+              </span>
+            </div>  
+            <div className={styles.homeMain__cardGamePosition}>top</div>
+          </div>      
+                
+          <ProgressBarFunding />
+        </div> {/* /card*/}
 
 
             
@@ -91,87 +65,64 @@ const HomeMain = () => {
 {/* Second row */}
  
 
-            <div className={styles.homeMain__cardContainer__card}> 
-            <div className={styles.homeMain__cardContainer__card_gameInfo}>
-              <div className={styles.homeMain__cardContainer__card_gameNameBlock}>
-                <h2 className={styles.homeMain__cardContainer__card_gameName}>
-                  Loftia VR Game
-                </h2>
-                <span className={styles.homeMain__cardContainer__card_videogame}>
-                  video game
-                </span>
-              </div> {/*  /gameNameBlock*/}
-              <div className={styles.homeMain__cardContainer__card_position}>top</div>
-            </div>  {/*  /gameInfo*/}    
-                 
-            <div className={styles.homeMain__cardContainer__card_gameStatus}>
-              <div className={styles.homeMain__cardContainer__card_funded}>
-                <span>funded</span>
-                <span>15 backers</span>
-              </div>
-              <span>34 days to go</span>
-            </div>
-          </div> {/* /card*/}
+<div className={styles.homeMain__card}> 
+          <div className={styles.homeMain__cardHeader}>
+            <div className={styles.homeMain__cardGameNameBlock}>
+              <h2 className={styles.homeMain__cardGameName}>
+                Loftia VR Game
+              </h2>
+              <span className={styles.homeMain__cardVideogame}>
+                video game
+              </span>
+            </div>  
+            <div className={styles.homeMain__cardGamePosition}>top</div>
+          </div>      
+                
+          <ProgressBarFunding />
+        </div> {/* /card*/}
 
 
 
-          <div className={styles.homeMain__cardContainer__card2}> 
-            <div className={styles.homeMain__cardContainer__card_gameInfo}>
-              <div className={styles.homeMain__cardContainer__card_gameNameBlock}>
-                <h2 className={styles.homeMain__cardContainer__card_gameName}>
-                Design System
-                <br />
-                Automation
-                </h2>
-                <span className={styles.homeMain__cardContainer__card_videogame}>
-                  video game
-                </span>
-              </div> {/*  /gameNameBlock*/}
-              <div className={styles.homeMain__cardContainer__card_position}>top</div>
-            </div>  {/*  /gameInfo*/}    
-                 
-            <div className={styles.homeMain__cardContainer__card_gameStatus}>
-              <div className={styles.homeMain__cardContainer__card_funded}>
-                <span>funded</span>
-                <span>15 backers</span>
-              </div>
-              <span>34 days to go</span>
-            </div>
-          </div> {/* /card*/}
+        <div className={styles.homeMain__card2}> 
+          <div className={styles.homeMain__cardHeader}>
+            <div className={styles.homeMain__cardGameNameBlock}>
+              <h2 className={styles.homeMain__cardGameName}>
+                Loftia VR Game
+              </h2>
+              <span className={styles.homeMain__cardVideogame}>
+                video game
+              </span>
+            </div>  
+            <div className={styles.homeMain__cardGamePosition}>top</div>
+          </div>      
+                
+          <ProgressBarFunding />
+        </div> {/* /card*/}
 
 
 
 
-
-          <div className={styles.homeMain__cardContainer__card}> 
-            <div className={styles.homeMain__cardContainer__card_gameInfo}>
-              <div className={styles.homeMain__cardContainer__card_gameNameBlock}>
-                <h2 className={styles.homeMain__cardContainer__card_gameName}>
-                  Loftia VR Game
-                </h2>
-                <span className={styles.homeMain__cardContainer__card_videogame}>
-                  video game
-                </span>
-              </div> {/*  /gameNameBlock*/}
-              <div className={styles.homeMain__cardContainer__card_position}>top</div>
-            </div>  {/*  /gameInfo*/}    
-                 
-            <div className={styles.homeMain__cardContainer__card_gameStatus}>
-              <div className={styles.homeMain__cardContainer__card_funded}>
-                <span>funded</span>
-                <span>15 backers</span>
-              </div>
-              <span>34 days to go</span>
-            </div>
-          </div> {/* /card*/}
+        <div className={styles.homeMain__card}> 
+          <div className={styles.homeMain__cardHeader}>
+            <div className={styles.homeMain__cardGameNameBlock}>
+              <h2 className={styles.homeMain__cardGameName}>
+                Loftia VR Game
+              </h2>
+              <span className={styles.homeMain__cardVideogame}>
+                video game
+              </span>
+            </div>  
+            <div className={styles.homeMain__cardGamePosition}>top</div>
+          </div>      
+                
+          <ProgressBarFunding />
+        </div> {/* /card*/}
 
 
 
 
 
 
-
-        </div>
       </section>
     </>
   );
