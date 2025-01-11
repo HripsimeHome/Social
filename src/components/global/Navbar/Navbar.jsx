888d480107cce4362a91e02f70737e8c8d7912c4
Hripsime <m_hripsime@mail.ru>
2025-01-11 20:11:47 +0400

    Added activ class to menu

--- src/components/global/Navbar/Navbar.jsx
+++ src/components/global/Navbar/Navbar.jsx
@@ -1,16 +1,11 @@
 import styles from "./Navbar.module.scss";
 import { NavLink } from "react-router-dom";
 import { menuItems } from "../../../constants/menuItems";
-
 import { avatarImage } from "../../../assets/images";
 
 const Navbar = () => {
-  //const location = useLocation();
-
   return (
-    <>
-      <div className={styles.navbar}>
-
+    <div className={styles.navbar}>
       <div className={styles.navbar__container}>
         <img
           src={avatarImage}
@@ -20,14 +15,21 @@ const Navbar = () => {
 
         <nav className={styles.navbar__menu}>
           {menuItems.map(({ link, text }, index) => (
-            <NavLink to={link} key={index} className={styles.navbar__menuLink}>              
+            <NavLink
+              to={link}
+              key={index}
+              className={({ isActive }) =>
+                isActive
+                  ? `${styles.navbar__menuLink} ${styles.navbar__menuLink_active}`
+                  : styles.navbar__menuLink
+              }
+            >
               {text}
             </NavLink>
           ))}
-        </nav>        
+        </nav>
       </div>
-      </div>
-    </>
+    </div>
   );
 };
 
