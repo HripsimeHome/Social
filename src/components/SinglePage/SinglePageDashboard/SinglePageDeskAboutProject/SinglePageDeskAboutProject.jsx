f6d1e4f508c65833254bcb552bda2950222e55e7
Hripsime <m_hripsime@mail.ru>
2025-01-18 20:34:36 +0400

    Aded customclass prop for percent

--- src/components/SinglePage/SinglePageDashboard/SinglePageDeskAboutProject/SinglePageDeskAboutProject.jsx
+++ src/components/SinglePage/SinglePageDashboard/SinglePageDeskAboutProject/SinglePageDeskAboutProject.jsx
@@ -1,34 +1,40 @@
-import styles from "./SinglePageDeskAboutProject.module.scss"
-import  LabelPin  from "../../../layout/LabelPin/LabelPin"
+import { useState } from "react";
+import styles from "./SinglePageDeskAboutProject.module.scss";
+import ProjectStatusLabel from "../../../layout/ProjectStatusLabel/ProjectStatusLabel";
 
 const SinglePageDeskAboutProject = () => {
+
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleReadMore = () => setIsExpanded((prev) => !prev);
+
   return (
-    <>
-      <section className={styles.singlePageDeskAboutProject}>
-      <div className={styles.singlePageDeskAboutProject__newPinContainer}>
-
-      {/*<div className={styles.singlePageDeskAboutProject__newPin}>new</div>*/}
-      <LabelPin />
-
+    <section className={styles.singlePageDeskAboutProject}>
+      <div className={styles.singlePageDeskAboutProject__projectStatusLContainer}>
+        <ProjectStatusLabel />
+      </div>
+      <div className={`${styles.singlePageDeskAboutProject} desk`}>
+        <h3 className="deskTitle">About Project</h3>
+        <p className={styles.singlePageDeskAboutProject__deskText}>
+          The exciting new VR card game Loftia.
+        </p>
+        <div
+          className={`${styles.singlePageDeskAboutProject__expandedText} ${
+            isExpanded ? styles.expanded : styles.collapsed
+          }`}
+         // aria-expanded={isExpanded} // Accessibility for screen readers
+        >
+          Loftia, all of the cards can be played as either a champion to fight for you on the destination. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
         </div>
-        <div className={`${styles.singlePageDeskAboutProject} desk`}>
-          <h3 className="deskTitle">
-          About Project
-          </h3>
-          <p className={styles.singlePageDeskAboutProject__deskText}>
-          The exciting new VR card game Loftia. 
-          </p>
-          <p className={styles.singlePageDeskAboutProject__truncate}>
-          Loftia, all of the  cards can be played as either a champion to fight for you on the destination
-                  
-          </p>
-          <span className={styles.singlePageDeskAboutProject__readMore}>read all
-          </span>
-          
-        </div>
-      </section>
-    </>
+        <button
+          className={styles.singlePageDeskAboutProject__readMore}
+          onClick={toggleReadMore}
+         // aria-label={isExpanded ? "Read short" : "Read all"} // Accessibility
+        >
+          {isExpanded ? "read short" : "read all"}
+        </button>
+      </div>
+    </section>
   );
 };
 
-export default SinglePageDeskAboutProject
+export default SinglePageDeskAboutProject;
--- src/components/layout/ProjectStatusLabel/ProjectStatusLabel.jsx
+++ src/components/layout/ProjectStatusLabel/ProjectStatusLabel.jsx
@@ -1,28 +1,28 @@
-import styles from "./LabelPin.module.scss";
+import styles from "./ProjectStatusLabel.module.scss";
 import { useState, useEffect } from "react"
 
-const LabelPin = () => {
-  const labels = ["new", "top", "popular"]; 
- 
+const ProjectStatusLabel = () => {
+
+  const labels = ["new", "top", "popular"];  
   const [currentLabelIndex, setCurrentLabelIndex] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentLabelIndex((prevIndex) => (prevIndex + 1) % labels.length);
-    }, 2000); // Change every 5 seconds
+    }, 2000); 
 
-    return () => clearInterval(intervalId); // Clean up on unmount
+    return () => clearInterval(intervalId); 
   }, []);
+ 
 
   return (
     <>
-      <div className={styles.labelPin}>     
-      
+      <div className="labelPin"> 
         <span>{labels[currentLabelIndex]}</span>
-
+        
       </div>     
     </>
   );
 };
 
-export default LabelPin;
+export default ProjectStatusLabel;
