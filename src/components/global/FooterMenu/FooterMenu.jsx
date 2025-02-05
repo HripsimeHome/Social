2f1368e57cedfc4442ee60090b39e6836380163f
Hripsime <m_hripsime@mail.ru>
2025-02-05 17:32:07 +0400

    Added Plus animation to the Footer menu icons

--- src/components/global/FooterMenu/FooterMenu.jsx
+++ src/components/global/FooterMenu/FooterMenu.jsx
@@ -1,6 +1,7 @@
 import styles from "./FooterMenu.module.scss"
 import Svg from "../../layout/Svg/Svg";
 import { NavLink } from "react-router-dom";
+import Lottie from "lottie-react"
 
 import {    
   homePagePath
@@ -15,48 +16,49 @@ import {
   stackbitIcon
 } from "../../../assets/svg";
 
+import {
+  plusAnim  
+} from "../../../assets/lotties"
+
+
 const footerMenuIcons = [
   {
     link: homePagePath,
     icon: moneyIcon
+  },  
+  {
+    link: "https://coino-rust.vercel.app",
+    //icon: stackbitIcon,
+    animation: plusAnim,
+    text: "STAKE"
   },
   {
     link: "https://crypto-tg-bot.vercel.app",
-    icon: futuresIcon
-  },
-  {
-    link: "https://crypto-tg-bot.vercel.app/orders",
-    icon: positionIcon
-  },
-  {
-    link: "https://crypto-tg-bot.vercel.app/currencies",
-    icon: crypto500Icon
-  },
-  {
-    link: "https://crypto-tg-bot.vercel.app/profile",
-    icon: usdtIcon
-  },
-  {
-    link: "https://celadon-meerkat-6d4c5a.netlify.app",
-    icon: stackbitIcon
+    icon: futuresIcon,   
   },
 ]
  
 const Footer = () => {
   return (
     <footer className={styles.footerMenu}>  
-
-      {footerMenuIcons.map(({ link, icon } , index) => (
-        <NavLink
-          to={link}
-          key={index}
-          className="footerMenu">
-          <Svg 
-            id={icon}
-            className={styles.footerMenu__btnImg}
-          />
-        </NavLink>
-      ))}
+      {footerMenuIcons.map(({ link, icon, animation, text } , index) => (
+        <NavLink to={link} key={index} className="footerMenu">
+        {animation ? (
+          <>
+            <Lottie
+              animationData={animation}
+              loop={true}
+              className={styles.footerMenu__plusAnim}
+            />
+            <span className={styles.footerMenu__switchText}>
+              {text}
+            </span>
+          </>
+        ) : (
+          <Svg id={icon} className={styles.footerMenu__btnImg} />
+        )}
+      </NavLink>
+      ))}          
     </footer>
   );
 };
